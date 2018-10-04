81806d418d627409b2fd4215b702954cd6957b6c
Kenny Chen <kc5by@virginia.edu>
2018-10-04 13:39:45 -0400

    Added Sounds and Started Spell Tree

---
+++ BardsTale/Assets/Scripts/spellTree.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// JKL; (light)
+// KJLJ; (firebolt)
+// LK;JK; (health)
+// LLLKL;J (super mario)
+// ;;;KLLLJ (Bethoveen's 5th)
+
+// space to cast
+
+public class SpellCasting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
